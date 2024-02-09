aa251154c764a84da6ab74c874236f340286789d
Pol9lk <laa2610@gmail.com>
2024-02-09 19:49:12 +0300

    new_1

--- handlers/custom_handlers/user/anketa.py
+++ handlers/custom_handlers/user/anketa.py
@@ -19,7 +19,31 @@ def getting_theme(message: Message):
     bot.delete_state(message.from_user.id, message.chat.id)
 
     bot.send_message(message.chat.id, "Напишите тему сообщения")
+    bot.set_state(message.from_user.id, Feedback.email)
+
+
+@bot.message_handler(func=None, state=Feedback.email, regexp="^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")
+def getting_true_email(message: Message):
+    """
+    Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
+
+    :param message: Message
+    :return:
+    """
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data["email"] = message.text
     bot.set_state(message.from_user.id, Feedback.commit)
+    bot.send_message(message.chat.id, "Напишите вашу почту")
+
+@bot.message_handler(func=None, state=Feedback.email)
+def getting_false_email(message: Message):
+    """
+    Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
+
+    :param message: Message
+    :return:
+    """
+    bot.send_message(message.chat.id, "Неправильно ввели почту")
 
 
 @bot.message_handler(func=None, state=Feedback.commit)
@@ -30,37 +54,8 @@ def getting_comment(message: Message):
     :param message: Message
     :return:
     """
-    bot.set_state(message.from_user.id, Feedback.email)
-    bot.send_message(message.chat.id, "Напишите ваш отзыв")
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["subject"] = message.text
-
-
-@bot.message_handler(func=None, state=Feedback.email, regexp="^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")
-def getting_comment(message: Message):
-    """
-    Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
-
-    :param message: Message
-    :return:
-    """
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["decs"] = message.text
     bot.set_state(message.from_user.id, Feedback.feedback)
-    bot.send_message(message.chat.id, "Напишите вашу почту")
-
-@bot.message_handler(func=None, state=Feedback.email)
-def getting_comment(message: Message):
-    """
-    Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
-
-    :param message: Message
-    :return:
-    """
-    bot.send_message(message.chat.id, "Неправильно ввели почту")
-
-
-
+    bot.send_message(message.chat.id, "Напишите ваш отзыв")
 
 @bot.message_handler(func=None, state=Feedback.feedback)
 def send_email(message: Message):
@@ -72,7 +67,7 @@ def send_email(message: Message):
     """
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         msg = MIMEText(f'Пользователь - {message.from_user.first_name} {message.from_user.last_name} написал '
-                       f'следующий текст:\n{data["decs"]}\nСообщение пришло из телеграмм бота. Почта пользователя - {message.text}')
+                       f'следующий текст:\n{message.text}\nСообщение пришло из телеграмм бота. Почта пользователя - {data["decs"]}')
         msg['Subject'] = data["subject"]
         msg['From'] = 'telegram_bot'
         msg['To'] = EMAIL
