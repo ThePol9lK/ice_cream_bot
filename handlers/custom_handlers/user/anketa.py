7165768ca2a5c0b20148dc0394ef1aed77207049
Pol9lk <laa2610@gmail.com>
2024-02-09 20:13:13 +0300

    new_3

--- handlers/custom_handlers/user/anketa.py
+++ handlers/custom_handlers/user/anketa.py
@@ -21,7 +21,7 @@ def getting_theme(message: Message):
     bot.set_state(message.from_user.id, Feedback.email)
 
 
-@bot.message_handler(func=None, state=Feedback.email, regexp=r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")
+@bot.message_handler(func=None, state=Feedback.email)
 def getting_true_email(message: Message):
     """
     Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
@@ -34,20 +34,8 @@ def getting_true_email(message: Message):
     bot.set_state(message.from_user.id, Feedback.commit)
     bot.send_message(message.chat.id, "Напишите вашу почту")
 
-@bot.message_handler(func=None, state=Feedback.email)
-def getting_false_email(message: Message):
-    """
-    Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
 
-    :param message: Message
-    :return:
-    """
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data["subject"] = message.text
-    bot.send_message(message.chat.id, "Неправильно ввели почту")
-
-
-@bot.message_handler(func=None, state=Feedback.commit)
+@bot.message_handler(func=None, state=Feedback.commit, regexp=r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")
 def getting_comment(message: Message):
     """
     Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
@@ -59,6 +47,17 @@ def getting_comment(message: Message):
         data["email"] = message.text
     bot.set_state(message.from_user.id, Feedback.feedback)
     bot.send_message(message.chat.id, "Напишите ваш отзыв")
+
+
+@bot.message_handler(func=None, state=Feedback.commit)
+def getting_comment(message: Message):
+    """
+    Сохранение значения темы сообщения. Отлавливается через состояние Feedback.commit.
+
+    :param message: Message
+    :return:
+    """
+    bot.send_message(message.chat.id, "Неправильно введена почта")
 
 @bot.message_handler(func=None, state=Feedback.feedback)
 def send_email(message: Message):
